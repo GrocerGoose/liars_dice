8ec1b40afebb8aa152e13020d47e9e168d7bf96e
Ben Schmeckpeper <ben@aisle50.com>
2013-06-25 16:09:48 -0500

    Add a separate BS object that subclasses Bid.

--- lib/bid.rb
+++ lib/bid.rb
@@ -1,5 +1,25 @@
-Bid = Struct.new(:total, :number) do
+class Bid
+  attr_accessor :total, :number
+
+  def init(total, number)
+    self.total = total
+    self.number = number
+  end
+
+  def bs_called?
+    false
+  end
+
   def to_s
     "#{total} #{number}s"
+  end
+end
+
+class BS < Bid
+  def init
+  end
+
+  def bs_called?
+    true
   end
 end
