48651bb0d58bed44305dfbbf898de2cdb603b150
Ben Schmeckpeper <ben@aisle50.com>
2013-06-25 15:58:49 -0500

    Rough draft of the main loop

--- engine.rb
+++ engine.rb
@@ -1,5 +1,5 @@
 class Engine
-  attr_accessor :seats
+  attr_accessor :seats, :starting_seat
 
   def roll
     seats.select(&:alive?).each do |seat|
@@ -10,6 +10,46 @@ class Engine
       end
 
       seat.dice = dice
+    end
+  end
+
+  def notify_bid(bid)
+    # TODO - send notification events
+    nil
+  end
+
+  def valid_bid?(bid, bs_allowed=true)
+    # TODO - validate bids
+    true
+  end
+
+  def round
+    # TODO - log bids
+    index = starting_seat
+    seat = seats[index]
+    bid = seat.bid
+    unless valid_bid?(bid, false)
+      raise StandardError("Invalid Bid")
+    end
+    notify_bid(bid)
+    index += 1
+
+    while True
+      index = 0 if index > seats.count
+      seat = seats[index]
+      if seat.lost?
+        index += 1
+        next
+      end
+
+      bid = seat.bid
+      if bid.bs_called?
+        notify_bs(bid)
+        break
+      end
+
+      notify_bid(bid)
+      index += 1
     end
   end
 
