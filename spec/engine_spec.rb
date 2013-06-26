c232f0508ef57d1169a52250bd4177bc74909931
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2013-06-25 21:18:58 -0500

    Specs for get_bid and roll_dice

--- spec/engine_spec.rb
+++ spec/engine_spec.rb
@@ -4,7 +4,73 @@ describe "Engine" do
   let(:engine) { Engine.new([], 5) }
   let(:seat) { Seat.new(0, nil, 5) }
 
-  describe "valid_bid?" do
+  describe "#get_bid" do
+    before do
+      seat.stub_chain(:player, :bid).and_return("bid")
+    end
+
+    it "gets a bid from the seat's user" do
+      engine.stub(:valid_bid?).and_return(true)
+      engine.get_bid(seat).should == "bid"
+    end
+
+    it "raises an error if given an invalid bid" do
+      engine.stub(:valid_bid?).and_return(false)
+      expect { engine.get_bid(seat) }.to raise_error
+    end
+  end
+
+  describe "#roll_dice" do
+    context "randomness" do
+      before do
+        # Roll the dice a bunch of times and capture the result in a hash
+        # keyed by die number
+        engine.stub(:alive_seats).and_return([seat])
+        seat.stub(:dice_left).and_return(6000)
+        rolled_dice = []
+        seat.stub(:dice=) { |val| rolled_dice = val }
+        engine.roll_dice
+        @histogram = Hash.new(0)
+        rolled_dice.each{|die| @histogram[die] += 1 }
+      end
+
+      it "chooses valid numbers" do
+        @histogram.keys.sort.should == [1,2,3,4,5,6]
+      end
+
+      it "randomly distributes numbers" do
+        6.times do |i|
+          @histogram[i+1].should > 800
+          @histogram[i+1].should < 1200
+        end
+      end
+    end
+
+    it "only rolls dice for seats that are alive" do
+      s1 = Seat.new(0, {}, 5)
+      s1.stub(:alive?).and_return(false)
+      s2 = Seat.new(1, {}, 5)
+      s2.stub(:alive?).and_return(true)
+      engine.stub(:seats).and_return([s1, s2])
+      s1.should_not_receive(:dice=)
+      s2.should_receive(:dice=)
+      engine.roll_dice
+    end
+
+    it "rolls the correct number of dice for each seat" do
+      s1 = Seat.new(0, {}, 5)
+      s1.stub(:dice_left).and_return(3)
+      s1.stub(:dice=) { |val| val.count.should == 3 }
+      s2 = Seat.new(1, {}, 5)
+      s2.stub(:dice_left).and_return(5)
+      s2.stub(:dice=) { |val| val.count.should == 5 }
+
+      engine.stub(:alive_seats).and_return([s1, s2])
+      engine.roll_dice
+    end
+  end
+
+  describe "#valid_bid?" do
     before do
       engine.stub(:previous_bid).and_return(nil)
     end
