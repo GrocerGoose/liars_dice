c498789e2fab5e2417f17b5af7a6373beae19924
Chris Doyle <archslide@gmail.com>
2013-07-02 13:49:49 -0500

    more comprehensive human bot output

--- lib/liars_dice/human_bot.rb
+++ lib/liars_dice/human_bot.rb
@@ -1,19 +1,36 @@
 module LiarsDice
   class HumanBot
-    attr_accessor :prev_bid, :dice
+    attr_accessor :prev_bid, :dice, :seat_number
 
     def initialize(seat_number, number_of_players, number_of_dice)
+      self.seat_number = seat_number
       puts "You're playing as HumanBot in seat #{seat_number}"
       puts "When asked for a bid, either enter <TOTAL> <FACE_VALUE> or BS"
     end
 
-
     def handle_event(event)
       if event.is_a? LiarsDice::BidMadeEvent
         self.prev_bid = event.bid
+        return if event.seat_number.to_i == self.seat_number.to_i
+        puts event.message
+      elsif event.is_a? LiarsDice::BSCalledEvent
+        return if event.seat_number.to_i == self.seat_number.to_i
+        puts event.message
       elsif event.is_a? LiarsDice::LoserEvent
+        puts "There were #{event.dice.flatten.select{|i| i == prev_bid.face_value or i == 1}.length} #{prev_bid.face_value}s"
+        if event.seat_number == self.seat_number
+          puts "You lose a die"
+        else
+          puts event.message
+        end
+        puts
         self.prev_bid = nil
       end
+    end
+
+    def format_event(event)
+      return if event.seat_number.to_i == seat_number.to_i
+      event.message
     end
 
     def dice=(dice)
@@ -22,11 +39,7 @@ module LiarsDice
     end
 
     def bid
-      if prev_bid
-        puts "Previous bid was #{prev_bid}"
-      else
-        puts "You're bidding first"
-      end
+      puts "You're bidding first" unless prev_bid
 
       print "What do you bid? "
       bid_string = gets.chomp
