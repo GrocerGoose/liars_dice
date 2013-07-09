221d5dc78fd927c2d02351673d4440418af9afd4
Ben Schmeckpeper <ben@aisle50.com>
2013-07-09 17:21:17 -0500

    Simplify how CommandLineWatcher stores seat number mappings.

--- lib/liars_dice/command_line_watcher.rb
+++ lib/liars_dice/command_line_watcher.rb
@@ -2,7 +2,7 @@ module LiarsDice
   class CommandLineWatcher
     include Watcher
 
-    attr_accessor :names, :justification
+    attr_reader :names
 
     def initialize
       self.names = {}
@@ -12,17 +12,21 @@ module LiarsDice
       append_after_game lambda{|winner| puts "Game over.  #{name(winner)} wins." }
       append_after_round lambda{|loser| puts "#{name(loser)} loses a die" }
       append_after_invalid_bid lambda{|seat| puts "#{name(seat)} made an invalid bid" }
-      append_after_seats_assigned lambda{|assignments| assignments.each{|number, name| puts "Seat #{number}: #{name}" } }
-      append_after_seats_assigned method(:remember_names)
+      append_after_seats_assigned lambda{|assignments| self.names = assignments }
     end
 
     def name(seat_number, justified=false)
       (names[seat_number] || "Seat #{seat_number}").ljust(justified ? justification : 0)
     end
 
-    def remember_names(assignments)
-      self.justification = assignments.values.map{|klass| klass.length }.max
-      assignments.each{|number, name| self.names[number] = name }
+    def justification
+      @justification ||= names.values.map(&:length).max
+      @justification
+    end
+
+    def names=(value)
+      @names = value
+      @names.each{|number, name| puts "Seat #{number}: #{name}" }
     end
   end
 end
